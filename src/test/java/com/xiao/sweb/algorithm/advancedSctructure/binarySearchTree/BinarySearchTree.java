51ce0155a69d5192f596c62aa8e86f5eca3ce290
xiaoyu <xiaoyu@lrhealth.com>
2023-06-01 16:31:30 +0800

    二叉搜索树

--- src/test/java/com/xiao/sweb/algorithm/advancedSctructure/binarySearchTree/BinarySearchTree.java
+++ src/test/java/com/xiao/sweb/algorithm/advancedSctructure/binarySearchTree/BinarySearchTree.java
@@ -1,4 +1,43 @@
 package com.xiao.sweb.algorithm.advancedSctructure.binarySearchTree;
 
+import com.xiao.sweb.algorithm.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinarySearchTree {
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<>();
+        }
+        // 构建闭区间[1, n]组成的BST
+        return build(1, n);
+    }
+
+    private List<TreeNode> build(int lo, int hi) {
+        List<TreeNode> res = new LinkedList<>();
+        if (lo > hi) {
+            res.add(null);
+            return res;
+        }
+
+        // 穷举root所以节点可能
+        for (int i = lo; i <= hi; i++) {
+            List<TreeNode> leftTree = build(lo, i - 1);
+            List<TreeNode> rightTree = build(i + 1, hi);
+
+            // 给root节点穷举所有左右子树的组合
+            for (TreeNode left : leftTree) {
+                for (TreeNode right : rightTree) {
+                    // i作为根节点root的值
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(right);
+                }
+            }
+        }
+        return res;
+    }
 }
--- src/test/java/com/xiao/sweb/algorithm/violentSearch/backtrack/Backtrack.java
+++ src/test/java/com/xiao/sweb/algorithm/violentSearch/backtrack/Backtrack.java
@@ -1,4 +1,21 @@
 package com.xiao.sweb.algorithm.violentSearch.backtrack;
 
+import java.util.LinkedList;
+
 public class Backtrack {
+
+    public void backtrack(int[] nums, LinkedList<Integer> track) {
+        if (nums.length == track.size()){
+            return;
+        }
+        Integer[] a = track.toArray(new Integer[0]);
+        for (int i = 0; i < nums.length; i++) {
+            if (track.contains(nums[i])){
+                continue;
+            }
+            track.add(nums[i]);
+            backtrack(nums, track);
+            track.removeLast();
+        }
+    }
 }
