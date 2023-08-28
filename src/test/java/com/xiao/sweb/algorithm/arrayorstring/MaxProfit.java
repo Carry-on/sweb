5a13f9574a703dbc770cd3537c725820a4b146d5
xiao <2379259142@qq.com>
2023-08-29 00:03:11 +0800

    array and string

--- src/test/java/com/xiao/sweb/algorithm/arrayorstring/MaxProfit.java
+++ src/test/java/com/xiao/sweb/algorithm/arrayorstring/MaxProfit.java
@@ -4,17 +4,30 @@ package com.xiao.sweb.algorithm.arrayorstring;
  * 121. 买卖股票的最佳时机
  */
 public class MaxProfit {
+//    public int maxProfit(int[] prices){
+//        int n = prices.length;
+//        int max = 0;
+//        for (int i = n-1; i >= 0 ; i--) {
+//            for (int j = i - 1; j >= 0; j--) {
+//                int temp = prices[i] - prices[j];
+//               if (temp > max){
+//                   max = temp;
+//               }
+//            }
+//        }
+//        return max;
+//    }
+
     public int maxProfit(int[] prices){
-        int n = prices.length;
-        int max = 0;
-        for (int i = n-1; i >= 0 ; i--) {
-            for (int j = i - 1; j >= 0; j--) {
-                int temp = prices[i] - prices[j];
-               if (temp > max){
-                   max = temp;
-               }
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice){
+                minPrice = prices[i];
+            }else if(prices[i] - minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
             }
         }
-        return max;
+        return maxProfit;
     }
 }
---
+++ src/test/java/com/xiao/sweb/learn/array/RemoveElement.java
@@ -0,0 +1,10 @@
+package com.xiao.sweb.learn.array;
+
+/**
+ * 27. 移除元素
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        return 0;
+    }
+}
