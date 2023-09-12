f1004fdf9876fd6fb12b26793e128b7eb69b57d8
xiaoyu <xiaoyu@lrhealth.com>
2023-09-12 13:57:21 +0800

    生命游戏

---
+++ src/test/java/com/xiao/sweb/algorithm/matrix/GameOfLive.java
@@ -0,0 +1,50 @@
+package com.xiao.sweb.algorithm.matrix;
+
+/**
+ * 289. 生命游戏
+ */
+public class GameOfLive {
+    public void gameOfLive(int[][] board) {
+        int[] neighbors = {0, 1, -1};
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int liveNeighbors = 0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (!(neighbors[i] == 0 && neighbors[j] == 0)) {
+                            // 相邻位置的坐标
+                            int r = row + neighbors[i];
+                            int c = col + neighbors[j];
+                            // 查看相邻的细胞是否是活细胞
+                            if ((r < rows && r >= 0) && (c < cols && c >= 0) && (Math.abs(board[r][c]) == 1)) {
+                                liveNeighbors++;
+                            }
+                        }
+                    }
+                }
+
+                // 规则 1 或规则 3
+                if ((board[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = -1;
+                }
+                // 规则 4
+                if (board[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] > 0) {
+                    board[i][j] = 1;
+                } else {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+}
