39faaeebbae6281926306e0ce3b4b9508ba61a28
xiaoyu <xiaoyu@lrhealth.com>
2023-09-13 19:20:33 +0800

    dp

---
+++ src/test/java/com/xiao/sweb/algorithm/dynamicProgramming/twoDimensionalDP/MinimumTotal.java
@@ -0,0 +1,30 @@
+package com.xiao.sweb.algorithm.dynamicProgramming.twoDimensionalDP;
+
+import java.util.List;
+
+/**
+ * 120. 三角形最小路径和
+ */
+public class MinimumTotal {
+//    public int minimumTotal(List<List<Integer>> triangle){
+//        int res = 0;
+//        if (triangle.size() == 0){
+//            return res;
+//        }
+//        int min = Integer.MAX_VALUE;
+//        for (int i = 0; i < triangle.size(); i++) {
+//            List<Integer> cur = triangle.get(i);
+//            for (int j = 0; j < cur.size(); j++) {
+//                min = Math.min(min, cur.get(j));
+//            }
+//            res += min;
+//            min = Integer.MAX_VALUE;
+//        }
+//        return res;
+//    }
+    public int minimumTotal(List<List<Integer>> triangle){
+        if (triangle.size() == 0){
+            return 0;
+        }
+    }
+}
